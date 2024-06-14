332958d2ca646e4c0086243f222a00b5af2deaec
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-06-14 09:19:24 +0100

    debugging

--- app.js
+++ app.js
@@ -10,12 +10,23 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST"], // Specify allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+};
+
+app.use(cors(corsOptions));
 
 // console.log(`DB_PORT: ${process.env.DB_PORT}`);
 // console.log(`DB_HOST: ${process.env.DB_HOST}`);
 // console.log(`DB_PROTOCOL: ${process.env.DB_PROTOCOL}`);
 // console.log(`DATABASENAME: ${process.env.DATABASENAME}`);
+
+app.use((req, res, next) => {
+  console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("HELLO TOSIN");
